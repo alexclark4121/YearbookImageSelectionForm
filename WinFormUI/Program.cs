a5a1fde3c98f39d8b92b2e3dbaee921626b836f5
Alex Clark <alexclark4121@gmail.com>
2019-07-26 01:51:58 -0400

    Commit to submit to codelouisville

--- WinFormUI/Program.cs
+++ WinFormUI/Program.cs
@@ -9,7 +9,7 @@ namespace WinFormUI
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. Yeet.
         /// </summary>
         [STAThread]
         static void Main()
